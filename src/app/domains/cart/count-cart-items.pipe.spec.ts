44acdf4efbabec55a679c88c27d5fa6d8ba4c3c5
Maciej Walecki <maciej.walecki@ppuc.pl>
2023-02-21 11:31:58 +0100

    removed old tests

--- src/app/domains/cart/count-cart-items.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountCartItemsPipe } from './count-cart-items.pipe';
-
-describe('CountCartItemsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountCartItemsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/directives/no-spaces.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoSpacesDirective } from './no-spaces.directive';
-
-describe('NoSpacesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NoSpacesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
